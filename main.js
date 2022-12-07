83123d6abec96adc251c1beb66a0cee00860847c
SamLiu <amazing_sam@outlook.com>
2022-12-07 21:16:26 +0800

    feat: ✅0-1-3实现mini-vue雏形

--- main.js
+++ main.js
@@ -19,6 +19,7 @@
 // ## 自己实现
 import {Dep, samEffect, reactive} from "./core/index.js";
 
+// 值类型的响应式
 // const a = new Dep(10)
 // let b = 0
 //
@@ -28,13 +29,55 @@ import {Dep, samEffect, reactive} from "./core/index.js";
 // })
 // a.value = 20
 
-const user = reactive({
-  age: 10
-})
-let nextAge = 0
+// 对象类型的响应式
+// const user = reactive({
+//   age: 10
+// })
+// let nextAge = 0
+//
+// samEffect(() => {
+//   nextAge = user.age + 1
+//   console.log(nextAge)
+// })
+// user.age++
 
-samEffect(() => {
-  nextAge = user.age + 1
-  console.log(nextAge)
-})
-user.age++
+// 有dom的响应式demo
+// const context = reactive({
+//   count: 0
+// })
+//
+// window.context = context
+//
+// samEffect(() => {
+//   // ui
+// })
+
+// todo 优化点
+// 全量更新
+// ui不够跨平台
+const App = {
+  // template => render
+  render (context) {
+    samEffect(() => {
+      document.querySelector('#app').textContent = ''
+      const element = document.createElement('div')
+      const text = document.createTextNode('这是一个响应式count计数： ')
+      const text1 = document.createTextNode(context.obj.count)
+
+      element.append(text)
+      element.append(text1)
+      document.querySelector('#app').append(element)
+    })
+  },
+  setup () {
+    const obj = reactive({
+      count: 1
+    })
+    window.obj = obj
+
+    return {
+      obj
+    }
+  }
+}
+App.render(App.setup())
