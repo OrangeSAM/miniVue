a3f0853ebda5a917f61c56dc75f953e40d67535f
SamLiu <amazing_sam@outlook.com>
2022-12-21 21:44:24 +0800

    feat: ✅1-1 集成jest做单元测试

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+/**
+ * Author：
+ * Date: 22.12.21
+ * Description:
+ */
+module.exports = {
+  presets: [
+    ["@babel/preset-env", {targets: {node: "current"}}],
+    "@babel/preset-typescript"
+  ]
+}
